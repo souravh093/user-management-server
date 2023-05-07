ef4462a14700a7f08c139007803851ecebe7ba44
Sourav halder <souravh093@gmail.com>
2023-05-07 21:59:29 +0600

    some problem face

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // middleware
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-const user = [
-    {id: 1, name: 'Manna', email: 'mannainfo@gmail.com'},
-    {id: 2, name: 'Joshim', email: 'joshim@gmail.com'},
-    {id: 3, name: 'Sabnur', email: 'sabnurfo@gmail.com'},
-]
+const users = [
+  { id: 1, name: "Manna", email: "mannainfo@gmail.com" },
+  { id: 2, name: "Joshim", email: "joshim@gmail.com" },
+  { id: 3, name: "Sabnur", email: "sabnurfo@gmail.com" },
+];
 
+app.get("/", (req, res) => {
+  res.send("Users management server running");
+});
 
-app.get('/', (req, res) => {
-    res.send('Users management server running')
-})
+app.get("/users", (req, res) => {
+  res.send(JSON.stringify(users));
+});
 
-app.get('/user', (req, res) => {
-    res.send(JSON.stringify(user));
-})
+app.post("/users", (req, res) => {
+  console.log("post api hitting");
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser)
+});
 
 app.listen(port, () => {
-    console.log(`Server is running port: ${port}`);  
-})
+  console.log(`Server is running port: ${port}`);
+});
